c0ee73b61ed5e06439e2caa116a4128b16a808ef
ashuuu2409 <ashuchopra2409@gmail.com>
2025-04-30 11:35:41 +0530

    add new files

--- frontend/src/components/ViewBook.jsx
+++ frontend/src/components/ViewBook.jsx
@@ -11,7 +11,7 @@ const ViewBook = () => {
 
   const handleView = async () => {
     try {
-      const res = await axios.get('https://book-app-31ms.onrender.com/books');
+      const res = await axios.get('https://book-app-kvry.onrender.com/books');
       setBooks(res.data);
     } catch (err) {
       console.log(err);
